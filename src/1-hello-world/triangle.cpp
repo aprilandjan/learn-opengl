551948caf3129571fbc9f69e51297eaf0257ca29
aprilandjan <maybreezes1988@gmail.com>
2019-07-29 23:52:43 +0800

    feat: add gl tools source

--- src/1-hello-world/triangle.cpp
+++ src/1-hello-world/triangle.cpp
@@ -6,8 +6,12 @@
 #include "triangle.h"
 #include <GL/glut.h>
 
-// need <GLTools>, wtf...
-//GLShaderManager shaderManager;
+//  OpenGL Toolkits
+#include <GLTools.h>
+#include <GLShaderManager.h>
+
+GLBatch triangleBatch;
+GLShaderManager shaderManager;
 
 void handleSizeChange(int w, int h) {
     //  handle window size change event
@@ -24,6 +28,11 @@ void handleRender() {
             1.0f, 0.0f, 0.0f, 1.0f
     };
 
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+    triangleBatch.Draw();
+
+    //  Perform the buffer swap to display the back buffer
+    glutSwapBuffers();
 }
 
 void draw_triangle(int argc, char * argv[]) {
