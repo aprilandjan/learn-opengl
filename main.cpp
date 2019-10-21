337daf40753c5bc752e9984534932d87a8fe0956
aprilandjan <maybreezes1988@gmail.com>
2019-10-21 23:57:48 +0800

    update config

--- main.cpp
+++ main.cpp
@@ -3,26 +3,33 @@
  */
 //#include<windows.h>
 
-#include <GLTools.h>    //  openGL toolkit
-#include <GLShaderManager.h>    //   shader manager class
+//#include <GLTools.h>    //  openGL toolkit
+//#include <GLShaderManager.h>    //   shader manager class
+//
+////#ifdef __APPLE__
+////#include <glut/glut.h>  //  osx GLUT
+////#else
+////#define FREEGLUT_STATIC
+////#include <GL/glut.h>    //  windows GLUT
+////#endif
+//
+////#include <windows.h>  // For MS Windows
+//
+////#include "./src/1-hello-world/hello-world.h"
+////#include "./src/1-hello-world/triangle.h"
+//
+///* Main function: GLUT runs as a console application starting at main()  */
+//int main(int argc, char *argv[]) {
+//
+////    draw_hello_world(argc, argv);
+////    draw_triangle(argc, argv);
+//
+//    return 0;
+//}
 
-#ifdef __APPLE__
-#include <glut/glut.h>  //  osx GLUT
-#else
-#define FREEGLUT_STATIC
-#include <GL/glut.h>    //  windows GLUT
-#endif
+#include <GL/gl.h>
 
-//#include <windows.h>  // For MS Windows
-
-//#include "./src/1-hello-world/hello-world.h"
-//#include "./src/1-hello-world/triangle.h"
-
-/* Main function: GLUT runs as a console application starting at main()  */
-int main(int argc, char *argv[]) {
-
-//    draw_hello_world(argc, argv);
-//    draw_triangle(argc, argv);
-
+int main() {
+    glClear(GL_COLOR_BUFFER_BIT);
     return 0;
 }
